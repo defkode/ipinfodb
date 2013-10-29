ca040c27bd960c985c9e8beaffc0d338ab59346e
Paweł Gościcki <pawel.goscicki@gmail.com>
2013-10-29 11:11:17 +0100

    Move main library into lib/ipinfodb/lookup.rb

--- lib/ipinfodb.rb
+++ lib/ipinfodb.rb
@@ -1,27 +1,3 @@
 require 'httparty'
-
-module Ipinfodb
-  
-  class << self
-    attr_accessor :api_key
-  end
-  
-  include HTTParty
-  
-  base_uri "api.ipinfodb.com"
-
-  def self.lookup(ip)
-    params = {
-      :output => "json",
-      :key    => api_key,
-      :ip     => ip
-    }
-    
-    body = get("/v2/ip_query_country.php", :query => params).parsed_response
-    if body["Status"] == "OK"
-      body
-    else
-      raise ArgumentError, body["Status"]
-    end
-  end
-end
+require 'ipinfodb/version'
+require 'ipinfodb/lookup'
